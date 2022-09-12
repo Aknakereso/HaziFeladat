55b93c44a94b1f738e53c52292b8c4b33ba9a182
Aknakereso <113178747+Aknakereso@users.noreply.github.com>
2022-09-12 17:08:19 +0200

    Hello_Krumpli

---
+++ Assets/FeladatA1.cs
@@ -0,0 +1,16 @@
+
+using UnityEngine;
+
+class FeladatA1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int szam = 5;
+        float tort = 5.5f;
+        string szoveg = "string";
+
+        Debug.Log(tort);
+
+    }
+}
---
+++ Assets/ismetles.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+class ismetles : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Debug.Log("Hello Krumpli");
+
+        
+       
+
+
+
+        
+    
+
+        
+    }
+}
